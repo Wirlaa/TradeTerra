8d042f66fc176eca7b526492b548552976e23b95
Wirlaa <115629715+Wirlaa@users.noreply.github.com>
2025-11-26 20:52:28 +0200

    Followed backend tutorial (#1)
    I followed the backend tutorial and compared it with what I've learnt in Advanced Web Applications course.

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,9 @@
+import { Router } from 'express'
+const router = Router()
+
+router.get('/', (req, res) => {
+    res.send('this is userRoutes');
+})
+
+// module.exports = router
+export default router
