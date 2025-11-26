f68374d539f5d0ab50b5cfcb1b6c40e8786bc13e
Wirla <115629715+Wirlaa@users.noreply.github.com>
2025-11-26 22:06:18 +0200

    Connected to a cloud mongodb instance

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import mongoose from 'mongoose'
 import router from './routes/userRoutes.js' //must have .js when not using ts
 
 dotenv.config()
-const app = express()
 
+mongoose.connection.once("open", () => {console.log("Connected to MongoDB")})
+mongoose.connection.on('error', console.error.bind(console,'MongoDB connection error'))
+// mongoose.set('strictQuery', false) //true means that only fields defined in your schema can be used in queries
+mongoose.connect(process.env.MONGODB_URI)
+
+const app = express()
 app.use(express.json())
 app.use(cors({
     origin: process.env.ORIGIN,
