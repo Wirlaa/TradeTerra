fd9efbebb8e1a0a88aed81db5cda26b044ecad4c
Wirlaa <115629715+Wirlaa@users.noreply.github.com>
2025-12-08 14:43:49 +0200

    Integrated UI (#3)
    * Changed file structure
    * Moved API calls to server side
    * Added games UI
    * Added working games logic

---
+++ routes/viewRoutes.js
@@ -0,0 +1,18 @@
+import { Router } from 'express'
+import { fileURLToPath } from 'url'
+
+const router = Router()
+
+router.get('/', (req, res) => {
+    res.sendFile(fileURLToPath(new URL('../public/charts.html', import.meta.url)))
+})
+
+router.get('/charts', (req, res) => {
+    res.sendFile(fileURLToPath(new URL('../public/charts.html', import.meta.url)))
+})
+
+router.get('/games', (req, res) => {
+    res.sendFile(fileURLToPath(new URL('../public/games.html', import.meta.url)))
+})
+
+export default router
